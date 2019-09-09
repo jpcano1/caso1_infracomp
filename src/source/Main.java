81823ed068c89d98123f08ac7fc5a7390087cba8
JUAN PABLO CANO BUITRAGO <jp.cano@sis.virtual.uniandes.edu.co>
2019-09-09 13:55:13 -0500

    Fors

--- src/source/Main.java
+++ src/source/Main.java
@@ -5,10 +5,10 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Collections;
 
 public class Main
 {
-
 	private static String archivoDatos = ".data/DatosCaso1.txt";
 	public static Buffer buffer;
 	public static void main(String[] args) throws FileNotFoundException 
@@ -17,25 +17,34 @@ public class Main
 		{
 			FileReader fr = new FileReader(new File(archivoDatos));
 			BufferedReader br = new BufferedReader(fr);
-			
+
 			String nClientes = br.readLine().split(":")[1];
 			int  numeroClientes = Integer.parseInt(nClientes);
-			
+			Cliente[] clientes = new Cliente[numeroClientes];
+
 			String nServidores = br.readLine().split(":")[1];
 			int numeroServidores = Integer.parseInt(nServidores);
-			
+			Servidor[] servidores = new Servidor[numeroServidores];
 
 			String cClientes = br.readLine().split(":")[1];
-			int consultaClientes = Integer.parseInt(cClientes);
-			
+			int nConsultaClientes = Integer.parseInt(cClientes);
+
 			String tBuffer = br.readLine().split(":")[1];
 			int tamanioBuffer = Integer.parseInt(tBuffer);
-		
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+
+			for(int i = 0; i < numeroClientes; i++)
+			{
+				clientes[i] = new Cliente((i+1)*1000, nConsultaClientes);
+			}
+
+			for(int i = 0; i < numeroServidores; i++)
+			{
+				servidores[i] = new Servidor((i+1)*100);
+			}
+		} 
+		catch (IOException e) 
+		{
 			e.printStackTrace();
 		}
-
 	}
 }
